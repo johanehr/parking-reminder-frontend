00f43dde7bc25519c37b594fd863d04dc351d200
Johan Ehrenfors <johanehrenfors@hotmail.com>
2023-12-03 12:40:35 +0100

    add semi-working about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,6 @@
-export default function Home() {
+import 'tailwindcss/tailwind.css'
+
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
@@ -18,26 +20,22 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <p>This is where my map will go</p>
-      </div>
+        <h2>Om projektet</h2>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="./about"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Om projektet{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[45ch] text-sm opacity-50`}>
-            Mer information om tjänsten
-          </p>
-        </a>
+        <p>
+          Som många andra, så har jag och sambon glömt att flytta bilen inför en städdag, vilket resulterade i saftiga böter.
+          Vi ville ha ett sätt att dels veta vart vi borde ställa bilen för att ha så lång tid som möjligt,
+          och gärna att kunna få påminnelser om bilen skulle behöva flyttas. 
+        </p>
+
+        <p>
+          Att bygga en sådan applikation tar en del tid, som ni säkert förstår - men jag tänkte börja
+          med att helt enkelt bygga en enkel karta som dynamiskt visar vart det är lämpligast att parkera.
+        </p>
+
+        <p>
+          Om du har haft nytta av mitt projekt får du gärna Swisha några kronor på 0737600282 :)
+        </p>
       </div>
     </main>
   )
