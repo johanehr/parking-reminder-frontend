0f566fc649a7ea7e560a72b9465b3cdb91d09b7f
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-01 09:27:37 +0100

    notes on contributions :)

--- src/app/elements/header.tsx
+++ src/app/elements/header.tsx
@@ -14,8 +14,19 @@ export default function Header() {
         target="_blank"
         rel="author"
       >
-      Skapad av Johan Ehrenfors
+      Skapad av Johan Ehrenfors,
       </a>
+      <span className="ml-2">
+        <a
+          className="sm:ml-auto text-xs lg:pointer-events-auto pointer-events-none"
+          href="https://www.fullstackfrancis.com"
+          target="_blank"
+          rel="author"
+        >
+      bidrag av Francis Jones
+        </a>
+      </span>
+
     </div>
   )
 }
