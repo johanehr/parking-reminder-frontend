8b1ff01b93c73a055c256f9df2f9d3779bef34c2
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-04-15 20:09:54 +0200

    language update, few tweaks to privacy policy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,11 +10,11 @@ export default function About() {
 
       <div className="m-auto max-w-5xl w-full mt-12">
         <h1 className="text-2xl">
-          Om projektet
+          About the project
         </h1>
 
         <p className="mt-2">
-          Projektet är skapat av   <a
+          This project is created by<a
             className="lg:pointer-events-auto pointer-events-none italic hover:underline"
             href="https://www.johanehrenfors.se"
             target="_blank"
@@ -22,7 +22,7 @@ export default function About() {
           >
             Johan Ehrenfors&nbsp;
           </a>
-          med bidrag från&nbsp;
+          with contributions from&nbsp;
           <a
             className="lg:pointer-events-auto pointer-events-none italic hover:underline"
             href="https://www.fullstackfrancis.com"
@@ -35,8 +35,8 @@ export default function About() {
         </p>
 
         <p className="mt-2">
-          Som många andra har vi glömt att flytta bilen inför en städdag, vilket har resulterat i saftiga böter.
-          Vi ville snabbt kunna ta reda på vart vi kan låta bilen stå så länge som möjligt, men även ett smidigt sätt att få relevanta påminnelser när det är dags att flytta bilen igen.
+          Like many others, we've forgotten to move our car before a cleaning day, resulting in hefty fines.
+          We wanted to quickly find out where we could leave our car for as long as possible, as well as a convenient way to receive relevant reminders when it's time to move our car again.
         </p>
 
         {/* notifikationer kommer bli klara när vi lanserad detta   <p className="mt-2">
@@ -44,29 +44,27 @@ export default function About() {
             med att helt enkelt bygga en enkel karta som dynamiskt visar vart det är lämpligast att parkera just nu baserad på kommande städdagar och tider
           </p> */}
 
-        <h1 className="text-2xl mt-6">Om Påminnelser och Sekretess</h1>
-          <p className="mt-4">Så fungerar det:</p>
-          <p className="mt-2">Anpassade Påminnelser:</p> Vårt system skickar ut påminnelser baserat på de tider och platser du anger. Detta gör att du alltid får relevanta påminnelser och aldrig missar en städdag. Du kan själv välja hur långt i förväg du vill bli påmind, och om du vill bli påmind via SMS eller e-post.
-          <p className="mt-2">Personuppgifter:</p> Vi samlar endast in den information som är nödvändig för att skicka effektiva påminnelser. Detta inkluderar din e-postadress/telefonnummer och platsen där din bil är parkerad.
-          <p className="mt-2">Dataskydd:</p> Din information är säker hos oss. Vi använder ledande säkerhetsteknik för att skydda dina uppgifter och delar dem aldrig med tredje part utan ditt uttryckliga medgivande.
-          <p className="mt-2">Privacy policy:</p>Läs mer om vår integritetspolicy här:
+        <h1 className="text-2xl mt-6">Notifications and Privacy</h1>
+        <p className="mt-4 text-xl">How it works</p>
+        <p className="mt-2">Customised reminders:</p> Our system sends out reminders based on the times, locations and vehicle nickname you specify. This ensures you always receive relevant reminders and never miss a cleaning day. You can choose how far in advance you want to be reminded, and if you prefer to receive reminders via SMS or email
+        <p className="mt-2">Personal Data:</p> We only collect the information necessary to send effective reminders. This includes your email address/phone number and the location where your car is parked.
+        <p className="mt-2">Data Protection:</p> Your information is safe with us. We use leading security technologies to protect your data and never share it with third parties without your explicit consent.
+        <p className="mt-2">Privacy policy:</p>Read more about our data policy here:
         <PrivacyPolicyModal />
         <h1 className="text-2xl mt-6">
-          Tekniken bakom
+          Technology overview
         </h1>
 
         <p className="mt-2">
-          Appen är byggd i React, Next.js, TS och Tailwind CSS. Vi använder Google-cloud tasks och SendGrid för notifikationssystemet.
+          Our app is built using React, Next.js, TypeScript, and Tailwind CSS. We utilise Google Cloud Tasks and SendGrid for the notification system.
         </p>
 
         <h1 className="text-2xl mt-6">
-          Hjälp till
+          Join our effort
         </h1>
 
         <p className="mt-2">
-          Vill du hjälpa till att förbättra tjänsten,
-          antingen som utvecklare eller genom att lägga till fler områden, kanske även utanför Bergshamra?
-          Tveka inte att kontakta mig på johanehrenfors@hotmail.com!
+          Would you like to help enhance our service, either by contributing as a developer or by adding new locations, possibly even beyond Bergshamra? Please feel free to contact me at johanehrenfors@hotmail.com!
         </p>
       </div>
     </main >
