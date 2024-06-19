6cc85b1860d8c4d92ac18d342c00285eb765cdbd
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-06-19 21:05:59 +0200

    reformatted userInput and added logic moving reminder time to day before if user wishes, and back to default if user changes mind.

--- src/parking-locations/helper-functions/formHelpers.ts
+++ src/parking-locations/helper-functions/formHelpers.ts
@@ -1,24 +1,27 @@
-import { NotifDayBefore } from "../types";
+import { NotifDayBefore, UserInput } from "../types";
 
 
 export const handleToggleChange = (stateSetter: React.Dispatch<React.SetStateAction<any>>, value: boolean, identifier: string) => {
-    if (identifier === "notif") {
-        stateSetter((prevState: NotifDayBefore) => ({
-            ...prevState,
-            acceptedNotifDayBefore: value,
-        }));
-    } else if (identifier === "terms") {
-        stateSetter(value);
-    }
-};
+  if (identifier === "notif") {
+    stateSetter((prevState: NotifDayBefore) => ({
+      ...prevState,
+      acceptedNotifDayBefore: value,
+    }));
+  } else if (identifier === "terms") {
+    console.log("hello terms", value)
+    stateSetter((prevState: UserInput) => ({
+      ...prevState,
+      acceptTerms: value,
+    }));
+  }
+}
 
 export const handleSelectionChange = (value: string, stateSetter: React.Dispatch<React.SetStateAction<number>>) => {
-    stateSetter(parseInt(value)); //currently only using for number selection, refactor if more generalised is needed.
-  };
+  stateSetter(parseInt(value)); //currently only using for number selection, refactor if more generalised is needed.
+};
 
 
-  export const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>, setState: React.Dispatch<React.SetStateAction<any>>, state: any) => {
-    const { name, value } = e.target;
-    setState({ ...state, [name]: value });
-  };
-  
+export const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>, setState: React.Dispatch<React.SetStateAction<any>>, state: any) => {
+  const { name, value } = e.target;
+  setState({ ...state, [name]: value });
+};
