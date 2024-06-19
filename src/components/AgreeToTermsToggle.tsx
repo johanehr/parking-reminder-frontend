93b1148db4cff0ecb32d1b07536e19d65b0c71c0
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-06-19 13:45:56 +0200

    updates to toggles and ui

---
+++ src/components/AgreeToTermsToggle.tsx
@@ -0,0 +1,17 @@
+/* import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+
+interface IToggleProps {
+    text: string,
+    id: string,
+    
+}
+
+export function AgreeToTermsToggle({ text, id }: IToggleProps) {
+    return (
+        <div className="flex items-center space-x-2">
+            <Switch id={id} />
+            <Label htmlFor={id}>{text}</Label>
+        </div>
+    )
+} */
