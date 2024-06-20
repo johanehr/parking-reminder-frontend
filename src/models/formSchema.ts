64c52727bcbb1beaab886d72feae39ba19566400
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-06-20 11:35:01 +0200

    handled case where user tries to set notification on spot which is undergoing cleaning

--- src/models/formSchema.ts
+++ src/models/formSchema.ts
@@ -2,8 +2,11 @@ import { z } from 'zod';
 
 export const formSchema = z.object({
   email: z.string().email(),
-  carNickname: z.string().min(1),
-  acceptTerms: z.boolean(),
+  carNickname: z.string().min(1, { message: "Car Nickname is required" }),
+  acceptTerms: z.boolean().refine((val) => val === true, {
+    message: "Please accept the terms to continue with setting a notification",
+  }),
+  
   notificationDate: z.string().refine((date) => {
     return !isNaN(Date.parse(date));
   }, {
