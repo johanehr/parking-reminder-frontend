f04fd7d06b29f5593dc77cd21ef1fe2abe965fd7
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-07-01 22:41:17 +0200

    notification ui flow improvements. focus on notifs landing in unsociable hours. wip

--- src/components/NotificationModal.tsx
+++ src/components/NotificationModal.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select"
-import { AugmentedParkingLocationData, NotifDayBefore, UserInput } from "@/parking-locations/types"
+import { AugmentedParkingLocationData, NotifUnsocialHours, UserInput } from "@/parking-locations/types"
 import { useEffect, useState } from "react"
 import { DateTime } from "luxon"
 import { Switch } from "./ui/switch"
@@ -11,17 +11,19 @@ import { calculateReminderDate } from "@/parking-locations/helper-functions/calc
 import { handleChange, handleSelectionChange, handleToggleChange } from "@/parking-locations/helper-functions/formHelpers"
 import { z } from 'zod';
 import axios from 'axios';
-
 import { formSchema } from "@/models/formSchema"
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert"
+import { Link } from "lucide-react"
+
+//TODO identify and fix bug where "day before" and "early" notifs get mixed. Suspect synchronicity issue with state update.
 
 interface INotificationModalProps {
   location: AugmentedParkingLocationData
 }
 
 export default function NotificationModal({ location }: INotificationModalProps) {
-  const [notificationBuffer, setNotificationBuffer] = useState(30)
-  const [notifDayBefore, setNotifDayBefore] = useState(new NotifDayBefore(false, false))
+  const [notificationBuffer, setNotificationBuffer] = useState(1440)
+  const [notifUnsocHours, setNotifUnsocHours] = useState(new NotifUnsocialHours(false, false, undefined))
   const [userInput, setUserInput] = useState(new UserInput(
     "",
     "",
@@ -55,20 +57,30 @@ export default function NotificationModal({ location }: INotificationModalProps)
   };
 
 
+  const calculateNotifUnsocialHours = () => {
 
-  //TODO look at edge cases where cleaning is ongoing. 
 
-  const calculateNotifToDayBefore = () => {
-    if (notifDayBefore.acceptedNotifDayBefore && userInput.notificationDate) {
-      let cleaningTimeDayBefore = userInput.notificationDate.minus({ days: 1 }).set({ hour: 20, minute: 0, second: 0, millisecond: 0 });
-      setUserInput((prev) => ({ ...prev, notificationDate: cleaningTimeDayBefore }))
-    } else if (!notifDayBefore.acceptedNotifDayBefore) {
+    if (notifUnsocHours.acceptedUnsocialHours && userInput.notificationDate) {
+      if (userInput.notificationDate.hour >= 0 && userInput.notificationDate.hour <= 6) {
+        //seperating this logic in order to then update UI accordingly. 
+        setNotifUnsocHours((prev) => ({ ...prev, dayBefore: true }))
+      } else if (userInput.notificationDate.hour >= 22) {
+        setNotifUnsocHours((prev) => ({ ...prev, dayBefore: false }))
+      }
+      if (notifUnsocHours.dayBefore) {
+        let cleaningTimeDayBefore = userInput.notificationDate.minus({ days: 1 }).set({ hour: 20, minute: 0, second: 0, millisecond: 0 });
+        setUserInput((prev) => ({ ...prev, notificationDate: cleaningTimeDayBefore }))
+      } else if (!notifUnsocHours.dayBefore) {
+        let cleaningTimeSameDay = userInput.notificationDate.set({ hour: 20, minute: 0, second: 0, millisecond: 0 });
+        setUserInput((prev) => ({ ...prev, notificationDate: cleaningTimeSameDay }))
+      }
+    } else if (!notifUnsocHours.acceptedUnsocialHours) {
       setUserInput((prev) => ({ ...prev, notificationDate: location.nextCleaningTime }))
-      setNotificationBuffer(30)
+      setNotificationBuffer(1440)
     }
   }
 
-  useEffect(() => { calculateNotifToDayBefore() }, [notifDayBefore.acceptedNotifDayBefore])
+  useEffect(() => { calculateNotifUnsocialHours() }, [notifUnsocHours.acceptedUnsocialHours])
 
   useEffect(() => {
     if (location.nextCleaningTime) {
@@ -83,9 +95,9 @@ export default function NotificationModal({ location }: INotificationModalProps)
 
       const reminderHour = notificationDate.hour;
       if (reminderHour < 6 || reminderHour >= 21) {
-        setNotifDayBefore(prev => ({ ...prev, suggestNotifDayBefore: true }));
+        setNotifUnsocHours(prev => ({ ...prev, suggestUnsocialHours: true }));
       } else {
-        setNotifDayBefore(prev => ({ ...prev, suggestNotifDayBefore: false }));
+        setNotifUnsocHours(prev => ({ ...prev, suggestUnsocialHours: false }));
       }
       setUserInput(prev => ({
         ...prev,
@@ -94,7 +106,7 @@ export default function NotificationModal({ location }: INotificationModalProps)
 
     }
     else {
-      alert("we have no next cleaning time available for this parking spot") //TODO FJ FIX EDGECASE LOGIC NEXTCLEANING TIME IS NULL
+      alert("we have no next cleaning time available for this parking spot")
     }
   }, [notificationBuffer]);
 
@@ -102,6 +114,7 @@ export default function NotificationModal({ location }: INotificationModalProps)
     const error = errors.find(error => error.path.includes(path));
     return error ? error.message : null;
   };
+
 
 
   useEffect(() => { console.log(userInput) }, [userInput])
@@ -133,45 +146,42 @@ export default function NotificationModal({ location }: INotificationModalProps)
                 {errors.find(error => error.path.includes('carNickname')) && <p className="text-xs text-red-500">{getErrorMessage('carNickname')}</p>}
               </div>
               <div className="grid gap-2">
-                {!notifDayBefore.acceptedNotifDayBefore &&
+                {!notifUnsocHours.acceptedUnsocialHours &&
                   <>
-                    < Label htmlFor="notification-time">Notification Time</Label>
-                    <Select name="notification-time" defaultValue="30" onValueChange={(e) => handleSelectionChange(e, setNotificationBuffer)}>
+                    <Label htmlFor="notification-time">Notification Time</Label>
+                    <Select name="notification-time" defaultValue="1440" onValueChange={(e) => handleSelectionChange(e, setNotificationBuffer)}>
                       <SelectTrigger className="w-full">
                         <SelectValue placeholder="Select time" />
                       </SelectTrigger>
                       <SelectContent>
-                        <SelectItem value="15">15 minutes before</SelectItem>
-                        <SelectItem value="30">30 minutes before</SelectItem>
                         <SelectItem value="60">1 hour before</SelectItem>
-                        <SelectItem value="120">2 hours before</SelectItem>
-                        <SelectItem value="360">6 hours before</SelectItem>
+                        <SelectItem value="180">3 hours before</SelectItem>
                         <SelectItem value="720">12 hours before</SelectItem>
+                        <SelectItem value="1440">24 hours before</SelectItem>
+                        <SelectItem value="2000">idk hours before</SelectItem>
+
+                        <SelectItem value="2880">48 hours before</SelectItem>
                       </SelectContent>
                     </Select>
                   </>
                 }
-                {notifDayBefore.suggestNotifDayBefore && (
+                {notifUnsocHours.suggestUnsocialHours && (
                   <div className="flex space-x-2 items-center">
-                    {notifDayBefore.acceptedNotifDayBefore ? <p className="text-xs my-2"> We will notify you the day before you need to move, <span className="font-bold">at 20:00 on the {location.nextCleaningTime?.toLocaleString(DateTime.DATE_SHORT)}</span> as requested </p> : <p className="text-xs my-2">Your requested notification time lands is in unsociable hours, on the <span className="font-bold">{userInput.notificationDate?.toLocaleString(DateTime.DATETIME_MED)}</span>, shall we notify you the day before at 20:00 instead?</p>}
-                    <Switch
-                      id="notifDayBefore"
-                      defaultChecked={notifDayBefore.acceptedNotifDayBefore}
-                      onCheckedChange={(e: boolean) => handleToggleChange(setNotifDayBefore, e, "notif")}
-                    />
-                    <Label htmlFor="termsToggle">Accept day before reminder</Label>
+                    {!notifUnsocHours.acceptedUnsocialHours &&
+                      <><p className="text-xs my-2">Your requested notification time lands during unsociable hours, on the <span className="font-bold">{userInput.notificationDate?.toLocaleString(DateTime.DATETIME_MED)}</span>, shall we notify you at 20:00 instead on the {notifUnsocHours.dayBefore ? "day before" : "same day"}?</p>
+                        <Switch
+                          id="unsocialHours"
+                          defaultChecked={notifUnsocHours.acceptedUnsocialHours}
+                          onCheckedChange={(e: boolean) => handleToggleChange(setNotifUnsocHours, e, "notif")}
+                        />
+
+                        <Label htmlFor="termsToggle">Accept {notifUnsocHours.dayBefore ? "reminder on the day before" : "early reminder"}</Label>
+                      </>
+                    }
                   </div>
                 )}
               </div>
-              <div className="flex space-x-2 items-center">
-                <Switch
-                  id="acceptTerms"
-                  defaultChecked={userInput.acceptTerms}
-                  onCheckedChange={(e: boolean) => handleToggleChange(setUserInput, e, "terms")}
-                />
-                <Label htmlFor="termsToggle">Accept terms</Label>
-                {errors.find(error => error.path.includes('acceptTerms')) && <p className="text-xs text-red-500">{getErrorMessage('acceptTerms')}</p>}
-              </div>
+
               {isCleaningOngoing ?
                 <Alert variant="destructive">
                   <div className="h-4 w-4" />
@@ -180,10 +190,24 @@ export default function NotificationModal({ location }: INotificationModalProps)
                     Cleaning is currently ongoing at the chosen location. A notification cannot be set at this time.
                   </AlertDescription>
                 </Alert> :
-                <DialogDescription className="mx-2">You will recieve a notification to move on:<br /><span className="text-black">{userInput.notificationDate?.toLocaleString(DateTime.DATETIME_MED)}</span></DialogDescription>
+                <DialogDescription className="mx-2">You will recieve {!notifUnsocHours.acceptedUnsocialHours && "your notification"}
+                  {notifUnsocHours.acceptedUnsocialHours && !notifUnsocHours.dayBefore && <><span className="text-red-600 font-bold"> an early</span> <span> notification </span></>}
+                  {notifUnsocHours.acceptedUnsocialHours && notifUnsocHours.dayBefore && <span> a notification, the <span className="text-red-600 font-bold">day before </span></span>}
+
+                  on:<br /> <div className="flex items-center justify-content"><span className="text-black">{userInput.notificationDate?.toLocaleString(DateTime.DATETIME_MED)}</span>
+                    {notifUnsocHours.acceptedUnsocialHours &&
+                      <Button onClick={() => {
+                        setNotifUnsocHours((prev) => ({ ...prev, acceptedUnsocialHours: false }))
+                      }} variant={"outline"} className="text-red-600 ml-auto">
+                        Reset
+                      </Button>
+                    }
+                  </div>
+                </DialogDescription>
               }
             </div>
-            <DialogFooter className="flex space-y-4 items-center">
+            <DialogFooter className="flex items-center justify-content ">
+              <h4 className="text-xs mr-auto">By continuing you are accepting the <br /><Link>terms and conditions</Link></h4>
               <Button disabled={isCleaningOngoing} type="submit">Set Reminder</Button>
             </DialogFooter>
           </div>
