66f635c1c555972e891ebab20c7714e2b14d089a
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-07-30 08:49:09 +0200

    added basic auth

--- API/cronTest.ts
+++ API/cronTest.ts
@@ -1,6 +1,13 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
+const SCHEDULE_SECRET_TOKEN = process.env.SECRET_TOKEN;
+
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const token = req.headers['x-secret-token']
+    if (token !== SCHEDULE_SECRET_TOKEN) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
     console.log(`Cron job triggered at ${new Date().toISOString()}`)
     res.status(200).json({ message: 'Cronjob working as expected' })
 }
